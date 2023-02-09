f4c438b2d32475205db1fd8e5562872e168b4516
okanisis <cryptdon@protonmail.com>
2023-02-09 09:42:54 -0700

    solution: vehicle-purchase

--- exercises/concept/vehicle-purchase/vehicle-purchase.js
+++ exercises/concept/vehicle-purchase/vehicle-purchase.js
@@ -11,7 +11,7 @@
  * @returns {boolean} whether a license is required
  */
 export function needsLicense(kind) {
-  throw new Error('Please implement the needsLicense function');
+  return (kind === 'car' || kind === 'truck');
 }
 
 /**
@@ -23,7 +23,7 @@ export function needsLicense(kind) {
  * @returns {string} a sentence of advice which option to choose
  */
 export function chooseVehicle(option1, option2) {
-  throw new Error('Please implement the chooseVehicle function');
+  return (option1 < option2 ? option1 : option2)+ " is clearly the better choice.";
 }
 
 /**
@@ -35,5 +35,6 @@ export function chooseVehicle(option1, option2) {
  * @returns {number} expected resell price in the dealership
  */
 export function calculateResellPrice(originalPrice, age) {
-  throw new Error('Please implement the calculateResellPrice function');
+  const rate = age < 3 ? 0.8 : age > 10 ? 0.5 : 0.7; 
+  return originalPrice * rate;
 }
